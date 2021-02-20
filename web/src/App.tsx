8cc5aac7b7394586f10dc0b6011bd178a4971cbe
Leonardo <srleon19@hotmail.com>
2021-02-19 22:19:49 -0300

    Front-end da aplicação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
-        Edit <code>src/App.tsx</code> and save to
-        reload.
-        <h1>Hello World!</h1>
-      </header>
+    <div>
+      <Routes />
     </div>
   );
 }
